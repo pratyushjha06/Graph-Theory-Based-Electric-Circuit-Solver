81597de5439941901f7b5157fd3a68e9c6da7c91
Khushi5155 <kkaayana5155@gmail.com>
2026-03-18 18:26:40 +0530

    Implemented branch current calculation using Ohm's Law

--- integration/solver.py
+++ integration/solver.py
@@ -21,4 +21,19 @@ def compute_node_voltages(V, node_mapping):
     node_voltages[0] = 0.0
 
     return node_voltages
-    
+
+def compute_branch_currents(node_voltages, edges):
+    branch_currents = {}
+
+    for comp in edges:
+        name, n1, n2, value = comp
+
+        if name.startswith('R'):  # resistor
+            v1 = node_voltages.get(n1, 0)
+            v2 = node_voltages.get(n2, 0)
+
+            current = (v1 - v2) / value
+            branch_currents[name] = float(current)
+
+    return branch_currents
+       
